ca40e299499c8b6335fa2927ec35a4f5000dcfa5
Andrew Jackson <andrew.d.jackson@gmail.com>
2023-02-15 16:49:38 +0000

    windows build

--- Make.bat
+++
@@ -1,2 +0,0 @@
-cd app
-python -m nuitka --include-data-dir=static=static --include-data-dir=templates=templates --disable-console --onefile --windows-icon-from-ico=static/images/icon.png memsfcr.py
